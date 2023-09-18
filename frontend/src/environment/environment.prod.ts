003305bbd3eaf6c1444a852ba97d6a08b16ac9ca
prashantrrai <19ce073@gardividyapith.ac.in>
2023-09-18 19:02:14 +0530

    footer updated

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://eber.onrender.com',
+    // apiUrl: 'https://eber.onrender.com',
+    apiUrl: 'http://localhost:4000',
     apiKey: 'your-production-api-key',
     frontendUrl: 'https://eberride.netlify.app',
   };
